d81d1bcfe770c668a28b2314ebe51edb9489241b
konradi78 <102959617+konradi78@users.noreply.github.com>
2022-11-14 10:00:36 +0100

    Add files via upload

---
+++ func.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from pymongo import *
+
+db = MongoClient('mongodb://telrun:tr2022@ab88.space:27017/?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1').project
+
+int bugs_count = 10
+int duration_secs = 20
+
+ def func(bugs_count, duration_secs
+        ):
+
+db.project.update(
+bugs_count
+duration_secs)
+
+
+
+
